0b058960609b88ce552999f888c8563509f78292
Dominic Hock <d.hock@it-hock.de>
2023-01-10 15:38:18 +0100

    fix: compiling with client id and secret
    chore: add -v / --version to get the version

--- src/utils/flags.go
+++ src/utils/flags.go
@@ -8,7 +8,14 @@ var (
 
 	flagShortNo = flag.Bool("n", false, "Answer no to all questions")
 	flagNo      = flag.Bool("no", false, "Answer no to all questions")
+
+	flagShortVersion = flag.Bool("v", false, "Show the version of fpm")
+	flagVersion      = flag.Bool("version", false, "Show the version of fpm")
 )
+
+func FlagVersion() bool {
+	return *flagShortVersion || *flagVersion
+}
 
 func FlagYes() bool {
 	return *flagShortYes || *flagYes
